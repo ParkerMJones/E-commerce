1f40a62dd8895c29f8419293ed84e8140778e71a
Parker Jones <parker.matthewjones@gmail.com>
2021-07-13 21:58:59 -0500

    fixed put routes category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,7 +55,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update({
+  Category.update(req.body, {
     where: {
       id: req.params.id
     }
